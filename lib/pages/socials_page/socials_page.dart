4fe592dc7db968c0ebbf910350a7ec21e34a331e
Aryan <aupathak1234@gmail.com>
2024-05-06 22:33:28 +0530

    Added bottom text, added the skeleton for all pages.

--- lib/pages/socials_page/socials_page.dart
+++ lib/pages/socials_page/socials_page.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:website/main.dart';
+import 'package:website/widgets/page_switch_buttons.dart';
 
 class SocialsPage extends StatelessWidget {
   const SocialsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        //crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Align(
+            alignment: Alignment.topLeft,
+            child: Flexible(
+              flex: 2,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: screenheight! * 0.01,),
+                  const PageWidget(buttonText: "Home"),
+                  const PageWidget(buttonText: "Projects"),
+                  const PageWidget(buttonText: "Socials"),
+                  const PageWidget(buttonText: "Photos"),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
