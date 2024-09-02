4cdc409eb69d4e82f2532c611751659d682fed4a
Aryan <aupathak1234@gmail.com>
2024-09-03 00:57:06 +0530

    responsive photos page

--- lib/pages/photos_page/photos_page.dart
+++ lib/pages/photos_page/photos_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
-import 'package:website/main.dart';
 import 'package:website/widgets/image_pane.dart';
 import 'package:website/widgets/page_switch_buttons.dart';
 
@@ -12,7 +11,6 @@ class PhotosPage extends StatefulWidget {
 }
 
 class _PhotosPageState extends State<PhotosPage> {
-
   final _images = const [
     ImagePane(fileName: 'assets/images/20221023081459_IMG_0712.JPG'),
     ImagePane(fileName: 'assets/images/IMG_0738.JPG'),
@@ -33,52 +31,84 @@ class _PhotosPageState extends State<PhotosPage> {
 
   @override
   Widget build(BuildContext context) {
-    final double screenwidth = MediaQuery.of(context).size.width;
-    final double screenheight = MediaQuery.of(context).size.height;
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        //crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-        Flexible(
-          flex: 2,
-          child: Align(
-            alignment: Alignment.topLeft,
-            child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: screenheight! * 0.01,),
-              const PageWidget(buttonText: "Home"),
-              const PageWidget(buttonText: "Projects"),
-              const PageWidget(buttonText: "Socials"),
-              const PageWidget(buttonText: "Photos"),
-            ],
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final double screenwidth = constraints.maxWidth;
+        final double screenheight = constraints.maxHeight;
+        bool isMobile = screenwidth < 600;
+
+        return Scaffold(
+          backgroundColor: Colors.black,
+          drawer: isMobile
+              ? Drawer(
+            child: Container(
+              color: Colors.black,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 50),
+                child: ListView(
+                  padding: EdgeInsets.zero,
+                  children: const [
+                    PageWidget(buttonText: "Home"),
+                    PageWidget(buttonText: "Projects"),
+                    PageWidget(buttonText: "Socials"),
+                    PageWidget(buttonText: "Photos"),
+                  ],
+                ),
+              ),
+            ),
+          )
+              : null,
+          appBar: AppBar(
+            backgroundColor: Colors.black,
+            leading: isMobile
+                ? Builder(
+              builder: (context) => IconButton(
+                icon: const Icon(Icons.menu),
+                onPressed: () => Scaffold.of(context).openDrawer(),
+              ),
+            )
+                : null,
           ),
-          ),
-        ),
-        Flexible(
-          flex: 8,
-          child: SizedBox(
-            // height: screenheight! * 0.6,
-            width: screenwidth! * 0.7,
-            child: MasonryGridView.count(crossAxisCount: 3,
-            itemCount: 14,
-            mainAxisSpacing: 15,
-            crossAxisSpacing: 15,
-            itemBuilder: (context, index) {
-              if (index < _images.length) {
-                return _images[index];
-              } else {
-                return const Placeholder();
-              }
-            },
+          body: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                if (!isMobile)
+                  Flexible(
+                    flex: 2,
+                    child: Align(
+                      alignment: Alignment.topLeft,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(height: screenheight * 0.01),
+                          const PageWidget(buttonText: "Home"),
+                          const PageWidget(buttonText: "Projects"),
+                          const PageWidget(buttonText: "Socials"),
+                          const PageWidget(buttonText: "Photos"),
+                        ],
+                      ),
+                    ),
+                  ),
+                Flexible(
+                  flex: 8,
+                  child: MasonryGridView.count(
+                    crossAxisCount: isMobile ? 1 : 3,
+                    itemCount: _images.length,
+                    mainAxisSpacing: 15,
+                    crossAxisSpacing: 15,
+                    itemBuilder: (context, index) {
+                      return _images[index];
+                    },
+                  ),
+                ),
+              ],
             ),
           ),
-        )
-      ],
-      ),
+        );
+      },
     );
   }
 }
