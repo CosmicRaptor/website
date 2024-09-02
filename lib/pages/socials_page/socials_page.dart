6418401987017a58da3f1b4278e44a951ad015d3
Aryan <aupathak1234@gmail.com>
2024-09-03 00:52:41 +0530

    responsive socials page

--- lib/pages/socials_page/socials_page.dart
+++ lib/pages/socials_page/socials_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:website/main.dart';
 import 'package:website/widgets/page_switch_buttons.dart';
 import 'package:website/widgets/socials_buttons.dart';
 
@@ -10,65 +9,102 @@ class SocialsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final double screenwidth = MediaQuery.of(context).size.width;
     final double screenheight = MediaQuery.of(context).size.height;
+    bool isMobile = screenwidth < 600;
+
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        //crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Flexible(
-            flex: 2,
-            child: Align(
-              alignment: Alignment.topLeft,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(height: screenheight! * 0.01,),
-                  const PageWidget(buttonText: "Home"),
-                  const PageWidget(buttonText: "Projects"),
-                  const PageWidget(buttonText: "Socials"),
-                  const PageWidget(buttonText: "Photos"),
-                ],
-              ),
+      drawer: isMobile ? Drawer(
+        child: Container(
+          color: Colors.black,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 50),
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: const [
+                PageWidget(buttonText: "Home"),
+                PageWidget(buttonText: "Projects"),
+                PageWidget(buttonText: "Socials"),
+                PageWidget(buttonText: "Photos"),
+              ],
             ),
           ),
-          Flexible(
-              flex: 8,
-              child: Stack(
-                children: [
-                  Positioned(
-                    left: -400,
-                    right: 0,
-                    top: screenheight! / 2 - 200,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                          const SocialsButtons(buttonText: "Discord"),
-                          SizedBox(height: screenheight! * 0.05,),
-                          const SocialsButtons(buttonText: "Steam"),
-                          SizedBox(height: screenheight! * 0.05,),
-                          const SocialsButtons(buttonText: "GitHub"),
-                        ],
-                    ),
-                  ),
-                  Positioned(
-                      bottom: 10,
-                      left: -50,
-                      right: 0,
-                      child: Align(
-                          alignment: Alignment.bottomCenter,
-                          child: SizedBox(width: screenwidth! * 0.2, child: const Text("Made with ♥️ using Flutter", style: TextStyle(
-                            fontFamily: "Jetbrains-Mono",
-                            color: Colors.white38,
-                            fontSize: 15,
-                          ),),)
-                      )
-                  )
-                ],
-              )
+        ),
+      ) : null,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        leading: isMobile ? Builder(
+          builder: (context) => IconButton(
+            icon: const Icon(Icons.menu),
+            onPressed: () => Scaffold.of(context).openDrawer(),
           ),
-        ],
+        ) : null,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            if (!isMobile)
+              Flexible(
+                flex: 1,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: screenheight * 0.01,),
+                    const PageWidget(buttonText: "Home"),
+                    const PageWidget(buttonText: "Projects"),
+                    const PageWidget(buttonText: "Socials"),
+                    const PageWidget(buttonText: "Photos"),
+                  ],
+                ),
+              ),
+            Flexible(
+              flex: 10,
+              child: Align(
+                alignment: Alignment.center,
+                child: Stack(
+                  children: [
+                    Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text("Connect with Me", style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 40,
+                              fontFamily: 'Jetbrains-Mono'
+                          ),),
+                          SizedBox(height: screenheight * 0.05,),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const SocialsButtons(buttonText: "Discord"),
+                              SizedBox(height: screenheight * 0.05,),
+                              const SocialsButtons(buttonText: "Steam"),
+                              SizedBox(height: screenheight * 0.05,),
+                              const SocialsButtons(buttonText: "GitHub"),
+                            ],
+                          ),
+                          SizedBox(height: screenheight * 0.05,),
+                          const Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text("Made with ♥️ using Flutter", style: TextStyle(
+                                fontFamily: "Jetbrains-Mono",
+                                color: Colors.white38,
+                                fontSize: 15,
+                              ),),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/widgets/socials_buttons.dart
+++ lib/widgets/socials_buttons.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:website/main.dart';
 import 'package:website/misc/get_image_path.dart';
 import 'package:website/misc/open_url.dart';
 
@@ -13,54 +12,66 @@ class SocialsButtons extends StatefulWidget {
 
 class _SocialsButtonsState extends State<SocialsButtons> {
   bool isHovering = false;
+
   @override
   Widget build(BuildContext context) {
-    final double screenwidth = MediaQuery.of(context).size.width;
-    final double screenheight = MediaQuery.of(context).size.height;
-    return SizedBox(
-      width: screenwidth! * 0.2,
-      child: InkWell(
-        onTap: (){openUrl(widget.buttonText);},
-        onHover: (hover){
-          setState(() {
-            isHovering = hover;
-          });
-        },
-        child: AnimatedContainer(
-          duration: const Duration(milliseconds: 200),
-          curve: Curves.ease,
-          padding: EdgeInsets.all(isHovering ? 30 : 20),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(15),
-            border: Border.all(
-              color: isHovering ? const Color.fromRGBO(0, 255, 0, 1.0) : Colors.black,
-            ),
-            boxShadow: const [
-              BoxShadow(
-                blurRadius: 5,
-                spreadRadius: 10
-              )
-            ]
-            ),
-          child: Row(
-            children: [
-              CircleAvatar(
-                backgroundColor: Colors.black,
-                radius: 25,
-                child: Image.asset(getImagePath(widget.buttonText)),
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final double screenwidth = constraints.maxWidth;
+       // final double screenheight = constraints.maxHeight;
+        bool isMobile = screenwidth < 600;
+
+        return SizedBox(
+          width: isMobile ? screenwidth * 0.8 : screenwidth * 0.2,
+          child: InkWell(
+            onTap: () {
+              openUrl(widget.buttonText);
+            },
+            onHover: (hover) {
+              setState(() {
+                isHovering = hover;
+              });
+            },
+            child: AnimatedContainer(
+              duration: const Duration(milliseconds: 200),
+              curve: Curves.ease,
+              padding: EdgeInsets.all(isHovering ? 30 : 20),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                border: Border.all(
+                  color: isHovering ? const Color.fromRGBO(0, 255, 0, 1.0) : Colors.black,
+                ),
+                boxShadow: const [
+                  BoxShadow(
+                    blurRadius: 5,
+                    spreadRadius: 10,
+                  ),
+                ],
               ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 15),
-                child: Text(widget.buttonText, style: const TextStyle(
-                  fontSize: 25,
-                  color: Colors.white,
-                  fontFamily: 'Jetbrains-Mono'
-                ),),
+              child: Row(
+                children: [
+                  CircleAvatar(
+                    backgroundColor: Colors.black,
+                    radius: 25,
+                    child: Image.asset(getImagePath(widget.buttonText)),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 15),
+                    child: Text(
+                      widget.buttonText,
+                      style: const TextStyle(
+                        fontSize: 25,
+                        color: Colors.white,
+                        fontFamily: 'Jetbrains-Mono',
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
-          ),
-        ),
+        );
+      },
     );
   }
 }
