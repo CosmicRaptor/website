ae93bba7c9581bcc70cde842917a4fa1ecb8af72
Aryan <aupathak1234@gmail.com>
2024-11-21 20:08:48 +0530

    started rebuilding the site

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp.router(
       routerConfig: router,
       title: 'Website',
+      debugShowCheckedModeBanner: false,
     );
   }
 }
--- lib/pages/landing page/landing_page.dart
+++ lib/pages/landing page/landing_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:website/widgets/page_switch_buttons.dart';
 
 class LandingPage extends StatefulWidget {
   const LandingPage({super.key});
@@ -8,137 +7,165 @@ class LandingPage extends StatefulWidget {
   State<LandingPage> createState() => _LandingPageState();
 }
 
-class _LandingPageState extends State<LandingPage> {
-  bool isHovering1 = false;
-  bool isHovering2 = false;
-  bool isHovering3 = false;
-  bool isHovering4 = false;
+class _LandingPageState extends State<LandingPage>
+    with TickerProviderStateMixin {
+  late AnimationController controller;
+  late AnimationController _bannerController;
+  late Animation _textAnimation;
+  late Animation<Offset> _slidingAnimation;
+  final String _text =
+      "I'm passionate about making Linux accessible and fun for everyone. I'm a Flutter developer and DevOps enthusiast who thrives in collaborative environments. Whether you're building the next big app or just tinkering with Linux, I'm always down to chat, code, troubleshoot issues, and maybe even swap some photography tips along the way. Let's explore the world of tech and creativity together!";
+  bool isFinishedTextAnimation = false;
+  String buttonText = 'Destroy';
+  double _opacity = 0;
+
+  @override
+  void initState() {
+    controller = AnimationController(
+      duration: const Duration(seconds: 10),
+      vsync: this,
+    );
+    _bannerController = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    );
+
+    _textAnimation =
+        Tween(begin: 0.0, end: _text.length.toDouble()).animate(controller);
+    _slidingAnimation = Tween<Offset>(
+      begin: const Offset(1, 1),
+      end: const Offset(0, 0),
+    ).animate(
+        CurvedAnimation(parent: _bannerController, curve: Curves.easeInOut));
+    controller.forward();
+    _bannerController.forward();
+    controller.addListener(() {
+      if (controller.status == AnimationStatus.completed) {
+        setState(() {
+          isFinishedTextAnimation = true;
+          _opacity = 1;
+        });
+      }
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    final double screenwidth = MediaQuery.of(context).size.width;
-    final double screenheight = MediaQuery.of(context).size.height;
-    bool isMobile = screenwidth < 600;
-
+    final screenSize = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Colors.black,
-      drawer: isMobile
-          ? Drawer(
-              child: Container(
-                color: Colors.black,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 50),
-                  child: ListView(
-                    padding: EdgeInsets.zero,
-                    children: const [
-                      PageWidget(buttonText: "Home"),
-                      PageWidget(buttonText: "Projects"),
-                      PageWidget(buttonText: "Socials"),
-                      PageWidget(buttonText: "Photos"),
-                    ],
-                  ),
-                ),
-              ),
-            )
-          : null,
       appBar: AppBar(
         backgroundColor: Colors.black,
-        leading: isMobile
-            ? Builder(
-                builder: (context) => IconButton(
-                  icon: const Icon(Icons.menu),
-                  onPressed: () => Scaffold.of(context).openDrawer(),
-                ),
-              )
-            : null,
+        elevation: 0,
+        actions: [
+          IconButton(
+            color: Colors.grey,
+            onPressed: () {
+              // Navigator.pushNamed(context, '/projects');
+            },
+            icon: const Icon(Icons.menu),
+          ),
+        ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            if (!isMobile)
-              Flexible(
-                flex: 1,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(
-                      height: screenheight * 0.01,
-                    ),
-                    const PageWidget(buttonText: "Home"),
-                    const PageWidget(buttonText: "Projects"),
-                    const PageWidget(buttonText: "Socials"),
-                    const PageWidget(buttonText: "Photos"),
-                  ],
+      body: Stack(
+        children: [
+          SlideTransition(
+            position: _slidingAnimation,
+            child: Transform.rotate(
+              angle: 45 * 3.141592653589793 / 180,
+              child: Container(
+                //blue color with opacity 0.5
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(100),
+                  color: const Color.fromRGBO(0, 0, 255, 0.5),
                 ),
+                height: 1000,
+                width: screenSize.width,
               ),
-            Flexible(
-              flex: 10,
-              child: Align(
-                alignment: Alignment.center,
-                child: Stack(
+            ),
+          ),
+          Center(
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width * 0.8,
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Center(
-                      child: SingleChildScrollView(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            const Text(
-                              "Hey there! Welcome to my creative corner",
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 40,
-                                  fontFamily: 'Jetbrains-Mono'),
-                            ),
-                            const Center(
-                              child: Text("Just a silly site.",
-                                  style: TextStyle(
-                                      color: Colors.white38,
-                                      fontFamily: 'Jetbrains-Mono',
-                                      fontSize: 25)),
-                            ),
-                            SizedBox(
-                              height: screenheight * 0.05,
-                            ),
-                            const SizedBox(
-                              //width: screenwidth * 0.8,
-                              child: Text(
-                                "Demystifying the wonders of Linux!  I'm passionate about making Linux accessible and fun for everyone. I'm a Flutter developer and DevOps enthusiast who thrives in collaborative environments. Whether you're building the next big app or just tinkering with Linux, I'm always down to chat, code, troubleshoot issues, and maybe even swap some photography tips along the way.  Let's explore the world of tech and creativity together!",
-                                style: TextStyle(
-                                  color: Colors.white70,
-                                  fontFamily: 'Jetbrains-Mono',
-                                  fontSize: 15,
-                                ),
-                              ),
-                            ),
-                            SizedBox(
-                              height: screenheight * 0.05,
-                            ),
-                            const Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text(
-                                  "Made with ♥️ using Flutter",
-                                  style: TextStyle(
-                                    fontFamily: "Jetbrains-Mono",
-                                    color: Colors.white38,
-                                    fontSize: 15,
-                                  ),
-                                ),
-                              ],
-                            )
-                          ],
-                        ),
+                    const Text(
+                      "Hello, I'm Aryan",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 50,
                       ),
+                    ),
+                    AnimatedBuilder(
+                      animation: _textAnimation,
+                      builder: (context, widget) {
+                        return RichText(
+                          textAlign: TextAlign.center,
+                          text: TextSpan(
+                            style: const TextStyle(
+                              color: Colors.grey,
+                              fontSize: 30,
+                            ),
+                            text: _text.substring(
+                                0, _textAnimation.value.toInt()),
+                          ),
+                        );
+                      },
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    AnimatedOpacity(
+                      duration: const Duration(seconds: 2),
+                      curve: Curves.linear,
+                      opacity: _opacity,
+                      child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            foregroundColor: Colors.white,
+                            backgroundColor: Colors.blueAccent,
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 30, vertical: 15),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30),
+                            ),
+                            textStyle: const TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          onPressed: () {
+                            if (isFinishedTextAnimation) {
+                              if (buttonText == 'Destroy') {
+                                setState(() {
+                                  controller.duration =
+                                      const Duration(seconds: 2);
+                                  controller.reverse();
+                                  _bannerController.reverse();
+                                  buttonText = 'Rebuild';
+                                });
+                              } else {
+                                setState(() {
+                                  controller.duration =
+                                      const Duration(seconds: 10);
+                                  controller.forward();
+                                  _bannerController.forward();
+                                  buttonText = 'Destroy';
+                                });
+                              }
+                            }
+                          },
+                          child: Text(buttonText)),
                     ),
                   ],
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
